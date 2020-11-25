897617e93f171213774f9d057fe764845fadc5d8
Quentin <win.moi@hotmail.com>
2020-11-25 19:18:43 +0100

    Fix default value to active field in suggestion table

---
+++ RestServer/Migrations/20201125181454_AddDefaultValueToActiveFieldInSuggestionTable.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestServer.Migrations
+{
+    public partial class AddDefaultValueToActiveFieldInSuggestionTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nom",
+                table: "Suggestions",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Active",
+                table: "Suggestions",
+                type: "bit",
+                nullable: true,
+                defaultValue: true,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nom",
+                table: "Suggestions",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "Active",
+                table: "Suggestions",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true,
+                oldDefaultValue: true);
+        }
+    }
+}
