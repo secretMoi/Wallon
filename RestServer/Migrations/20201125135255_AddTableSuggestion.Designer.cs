df1eb6594ae61ee870eefaffb4f7833f5a90d986
Quentin <win.moi@hotmail.com>
2020-11-25 15:19:06 +0100

    Upgrade server to .NET 5 Ajout table suggestion

--- RestServer/Migrations/20201125135255_AddTableSuggestion.Designer.cs
+++ RestServer/Migrations/20201125135255_AddTableSuggestion.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RestServer.Data;
 
 namespace RestServer.Migrations
 {
     [DbContext(typeof(WallonsContext))]
-    partial class WallonsContextModelSnapshot : ModelSnapshot
+    [Migration("20201125135255_AddTableSuggestion")]
+    partial class AddTableSuggestion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RestServer/Migrations/20201125135255_AddTableSuggestion.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestServer.Migrations
+{
+    public partial class AddTableSuggestion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Suggestions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nom = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    LocataireId = table.Column<int>(nullable: true),
+                    Active = table.Column<bool>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Suggestions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Suggestions_Locataires_LocataireId",
+                        column: x => x.LocataireId,
+                        principalTable: "Locataires",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Suggestions_LocataireId",
+                table: "Suggestions",
+                column: "LocataireId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Suggestions");
+        }
+    }
+}
